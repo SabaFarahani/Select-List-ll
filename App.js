5ea5f33c6406ffcbae0275114f9d25ef702c0b02
Saba Farahani <sabafarahani5@gmail.com>
2022-05-10 20:24:19 +0430

    Update App.js

--- App.js
+++ App.js
@@ -4,22 +4,8 @@ import Job from "./Jobs";
 import datas from "./data.json";
 
 function App() {
-  const [jobs, setJobs] = useState([]);
   const [search, setSearch] = useState("");
 
-  /*const getData=()=>{
-    fetch(datas)
-      .then((resp) => {
-        setJobs(resp.json());
-      })
-      .catch((error) => console.log(error));
-  };
-  */
-  /*
-  useEffect(() => {
-    getData();
-  }, []);
-*/
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
@@ -61,34 +47,3 @@ function App() {
 }
 
 export default App;
-
-/*useEffect(() => {
-    axios
-      .get(
-        "data.json"
-      )
-      .then((resp) => {
-        setCoins(resp.data);
-      })
-      .catch((error) => console.log(error));
-  }, []);*/
-/*<div className="coin-app">
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-  };
-      {filteredCoins.map((coin) => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            symbol={coin.symbol}
-            marketcap={coin.market_cap}
-            price={coin.current_price}
-            priceChange={coin.price_change_percentage_24h}
-            volume={coin.total_volume}
-          />
-        );
-      })}
-      ;
-    </div>*/
